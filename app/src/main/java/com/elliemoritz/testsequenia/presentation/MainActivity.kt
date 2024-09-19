9892879cf54160db2d231f8ce6fc9ecd87ed883d
Elisa Moritz <dark.little.finch@gmail.com>
2024-09-19 23:44:50 +0700

    add snackbar with error

--- app/src/main/java/com/elliemoritz/testsequenia/presentation/MainActivity.kt
+++ app/src/main/java/com/elliemoritz/testsequenia/presentation/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.elliemoritz.testsequenia.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.elliemoritz.testsequenia.R
 import com.elliemoritz.testsequenia.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +15,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        showSnackbar()
+    }
+
+    private fun showSnackbar() {
+        val colorYellow = ContextCompat.getColor(this, R.color.yellow)
+
+        val snackbar = Snackbar.make(
+            binding.root,
+            R.string.toast_error,
+            Snackbar.LENGTH_INDEFINITE
+        )
+            .setActionTextColor(colorYellow)
+            .setAction(R.string.toast_repeat) {
+                Log.d("MainActivity", "Repeat operation")
+            }
+//            .show()
+
+        val snackbarView = snackbar.view
+        snackbarView.setPadding(PADDING_VALUE, PADDING_VALUE, PADDING_VALUE, PADDING_VALUE)
+
+        snackbar.show()
+    }
+
+    companion object {
+        private const val PADDING_VALUE = 16
     }
 }
