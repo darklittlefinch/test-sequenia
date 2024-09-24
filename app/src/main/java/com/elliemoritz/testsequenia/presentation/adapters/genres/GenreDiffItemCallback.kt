143fe1aa3a1d51f90f1a3d225c687de9ef370192
Elisa Moritz <dark.little.finch@gmail.com>
2024-09-25 05:58:44 +0700

    add genre selecting

--- app/src/main/java/com/elliemoritz/testsequenia/presentation/adapters/genres/GenreDiffItemCallback.kt
+++ app/src/main/java/com/elliemoritz/testsequenia/presentation/adapters/genres/GenreDiffItemCallback.kt
@@ -1,14 +1,15 @@
 package com.elliemoritz.testsequenia.presentation.adapters.genres
 
 import androidx.recyclerview.widget.DiffUtil
+import com.elliemoritz.testsequenia.domain.Genre
 
-object GenreDiffItemCallback : DiffUtil.ItemCallback<String>() {
+object GenreDiffItemCallback : DiffUtil.ItemCallback<Genre>() {
 
-    override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
-        return oldItem == newItem
+    override fun areItemsTheSame(oldItem: Genre, newItem: Genre): Boolean {
+        return oldItem.name == newItem.name
     }
 
-    override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+    override fun areContentsTheSame(oldItem: Genre, newItem: Genre): Boolean {
         return oldItem == newItem
     }
 }
--- app/src/main/java/com/elliemoritz/testsequenia/presentation/adapters/genres/GenresAdapter.kt
+++ app/src/main/java/com/elliemoritz/testsequenia/presentation/adapters/genres/GenresAdapter.kt
@@ -2,10 +2,13 @@ package com.elliemoritz.testsequenia.presentation.adapters.genres
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.ListAdapter
+import com.elliemoritz.testsequenia.R
 import com.elliemoritz.testsequenia.databinding.ItemGenreBinding
+import com.elliemoritz.testsequenia.domain.Genre
 
-class GenresAdapter : ListAdapter<String, GenreViewHolder>(GenreDiffItemCallback) {
+class GenresAdapter : ListAdapter<Genre, GenreViewHolder>(GenreDiffItemCallback) {
 
     var onGenreClickListener: OnGenreClickListener? = null
 
@@ -20,13 +23,22 @@ class GenresAdapter : ListAdapter<String, GenreViewHolder>(GenreDiffItemCallback
 
     override fun onBindViewHolder(holder: GenreViewHolder, position: Int) {
         val genre = getItem(position)
-        holder.binding.tvGenreName.text = genre
+        holder.binding.tvGenreName.text = genre.name
+
+        val colorResId = if (genre.selected) {
+            R.color.yellow
+        } else {
+            R.color.white
+        }
+        val color = ContextCompat.getColor(holder.binding.root.context, colorResId)
+        holder.binding.tvGenreName.setBackgroundColor(color)
+
         holder.binding.root.setOnClickListener {
             onGenreClickListener?.onGenreClick(genre)
         }
     }
 
     interface OnGenreClickListener {
-        fun onGenreClick(genre: String)
+        fun onGenreClick(genre: Genre)
     }
 }
